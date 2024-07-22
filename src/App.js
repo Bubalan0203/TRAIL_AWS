744c72b86c26c51432bb1bc2cc85ccd7aa30fc59
Bubalan0203 <bubalan28@gmail.com>
2024-07-22 11:22:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 // src/App.js
-import {useState} from 'react';
 
+import {useState} from 'react';
+import Gallery from './Gallery.js';
 
 function App() {
-    const [list,SetList] = useState([]);
-    const [count,SetCount] = useState(1)
-    
-
-    function addItem(){
-    
-
-      let itemName = 'Item'+ count;
-      SetCount((currentState)  => { return currentState+1 })
-      SetList((currentState) => { return [...currentState, itemName]})
-      
-      console.log('current state', list)
-
-  }
-
-
-  function removeItem(){
-    let itemName = 'Item'+ count;
-    SetCount((currentState)  => { return currentState-1 })
-    SetList((currentState) => { return [...currentState, itemName]})
-   
-   
-    
-
-
-    console.log('current state', list)
-    
-  }
-  
-
 
  return (
  
      <>
-       <h1> list </h1>
-       <button onClick={addItem} > add item</button>
-       <button onClick={removeItem} >  remove</button>
-       <ul> {
-        list.map((el,index) => <li key={index}>{el} </li> )
-       }
-       </ul>
+       <Gallery />
  
     </>
 
